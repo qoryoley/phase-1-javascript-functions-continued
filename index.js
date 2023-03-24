ccc0b727f0c3c4f1a87e7f50476ddff4c3f3f53b
Abdalla <qoryoley@gmail.com>
2023-03-24 10:19:10 +0300

    Completed Function Continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(someString = 'roller-skate'){
+    return `This Saturday, I want to ${someString}!`;
+}
+
+function mondayWork(someString = 'go to the office'){
+    return `This Monday, I will ${someString}.`;
+}
+function wrapAdjective(wrapper){
+    return function (someString){
+        return `You are ${wrapper}${someString}${wrapper}!`; 
+    }
+}
+
+console.log(wrapAdjective('||'));
